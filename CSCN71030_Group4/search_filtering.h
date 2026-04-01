a610648215a4fe7286a5a44a90d855abe3658394
Oluwaloni <ayenimidun@gmail.com>
2026-04-01 12:35:46 -0400

    added search filtering header and function declarations

--- CSCN71030_Group4/search_filtering.h
+++ CSCN71030_Group4/search_filtering.h
@@ -1,1 +1,7 @@
 #pragma once
+
+#include "item.h"
+
+Item* loadData(const char* filename, int* count);
+Item* filterByBudget(Item* list, int count, const char* category, float minBudget, float maxBudget, int* filteredCount);
+void freeResults(Item* list);
