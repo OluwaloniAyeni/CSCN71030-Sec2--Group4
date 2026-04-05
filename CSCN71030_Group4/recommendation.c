ca59d540ec4b5cbf05bde37fe592347c0dfbd0e1
Oluwaloni <ayenimidun@gmail.com>
2026-04-05 08:39:56 -0400

    implemented sortByPrice function

--- CSCN71030_Group4/recommendation.c
+++ CSCN71030_Group4/recommendation.c
@@ -24,3 +24,19 @@ Item* generateRecommendations(Item* filteredlist, int count, int* recommendation
 	*recommendationCount = count;
 	return recommended;
 }
+
+void sortByPrice(Item* list, int count)
+{
+	for (int i = 0; i < count - 1; i++) 
+	{
+		for (int j = 0; j < count - i - 1; j++) 
+		{
+			if (list[j].price > list[j + 1].price) 
+			{
+				Item temp = list[j];
+				list[j] = list[j + 1];
+				list[j + 1] = temp;
+			}
+		}
+	}
+}
