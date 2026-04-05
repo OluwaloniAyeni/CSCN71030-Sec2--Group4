bfbed269c35f63c13014f6dbf017bc06f145bfc7
Oluwaloni <ayenimidun@gmail.com>
2026-04-05 09:53:15 -0400

    implemented startProgram function in main contol.c

--- CSCN71030_Group4/main_control.c
+++ CSCN71030_Group4/main_control.c
@@ -4,3 +4,20 @@
 #include "search_filtering.h"
 #include "recommendation.h"
 #include "user_input.h"
+
+int startProgram(const char* filename)
+{
+	int count = 0;
+	Item* allItems = loadData(filename, &count);
+
+	if (allItems == NULL || count <= 0)
+	{
+		printf("Error: Could not load data from file:\n");
+		return 1;
+	}
+
+	manageFlow(allItems, count);
+
+	freeResults(allItems);
+	return 0;
+}
