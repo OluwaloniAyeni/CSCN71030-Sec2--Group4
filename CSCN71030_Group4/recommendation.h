e7af3decfa128939a679edee4d383ba7f3716e50
Oluwaloni <ayenimidun@gmail.com>
2026-04-05 08:32:45 -0400

    added recommendation header and function declarations

--- CSCN71030_Group4/recommendation.h
+++ CSCN71030_Group4/recommendation.h
@@ -1,1 +1,6 @@
 #pragma once
+#include "item.h"
+
+Item* generateRecommendations(Item* filteredlist, int count, int* recommendationCount);
+void sortByPrice(Item* list, int count);
+void freeRecommendations(Item* list);
