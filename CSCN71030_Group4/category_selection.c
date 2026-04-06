e935226c4ef0d941197086dba8eeac734c31ba0c
Oluwaloni <ayenimidun@gmail.com>
2026-04-06 15:54:38 -0400

    added processCategory function declarations to category_selection.c

--- CSCN71030_Group4/category_selection.c
+++ CSCN71030_Group4/category_selection.c
@@ -1,27 +1,27 @@
-#include<stdio.h>
+#include <string.h>
+#include "category_selection.h"
 
-void receiveData(Node* head) {
+const char* processCategory(const char* categoryInput)
+{
+	if (categoryInput == NULL)
+	{
+		return NULL;
+	}
 
-}
+	if (strcmp(categoryInput, "gym") == 0)
+	{
+		return "gym";
+	}
 
-Node* head searchCategory(Node* head, char* name) {
-	// Function to search for a specific category in the linked list based on the name field
-	// It traverses the linked list and compares the name of each node with the input name, returning the node if a match is found or indicating that the category was not found if no match is found
-	// This function allows the user to quickly find and access information about a specific category in the linked list
-	//this funtion passes node under one specific category to the recommendation system for further processing
-	return category;
-}
+	else if (strcmp(categoryInput, "hotel") == 0)
+	{
+		return "hotel";
+	}
 
-void addCategory(Node** head) {
-	// Function to add a new category to the linked list, allowing the user to input the name and budget for the new category, and then insert it into the linked list in the correct position based on alphabetical order
-	// It allows dynamic shrinking and expanding of the linked list as new categories are added or removed, and it ensures that the linked list remains sorted in alphabetical order after each insertion
-}
+	else if (strcmp(categoryInput, "restaurant") == 0)
+	{
+		return "restaurant";
+	}
 
-void deleteCategory(Node** head) {
-	// Function to delete a category from the linked list, allowing the user to input the name of the category to be deleted, and then remove it from the linked list while maintaining the sorted order
-	// It allows dynamic shrinking and expanding of the linked list as new categories are added or removed, and it ensures that the linked list remains sorted in alphabetical order after each deletion
-}
-
-void displayCategories(Node* head) {
-
+	return NULL;
 }
