1a93cca5812b96f2d287fcb2876b61270e8aa17a
Oluwaloni <ayenimidun@gmail.com>
2026-04-06 16:52:04 -0400

    added sortByPrice() function to sort items in ascending order based on price and rankByRating() to rank items by rating and use price as tie breaker

--- CSCN71030_Group4/sorting_and_ranking.c
+++ CSCN71030_Group4/sorting_and_ranking.c
@@ -1,1 +1,53 @@
-#include "ranking_and_sorting.h"
+#include "sorting_and_ranking.h"
+#include <cstddef>
+
+void sortByPrice(Item* items, int count)
+{
+
+	if (items == NULL || count <= 1) {
+		return;
+	}
+
+	for (int i = 0; i < count - 1; i++) 
+
+	{
+		for (int j = 0; j < count - i - 1; j++)
+	 {
+			if (items[j].price > items[j + 1].price) 
+			
+		{
+				Item temp = items[j];
+				items[j] = items[j + 1];
+				items[j + 1] = temp;
+		}
+		
+	 }
+	}
+}
+
+void rankByRating(Item* items, int count)
+{
+	if (items == NULL || count <= 1) {
+		return;
+	}
+	for (int i = 0; i < count - 1; i++) 
+	{
+		for (int j = 0; j < count - i - 1; j++)
+	 {
+			if (items[j].rating < items[j + 1].rating) 
+			
+		{
+				Item temp = items[j];
+				items[j] = items[j + 1];
+				items[j + 1] = temp;
+		}
+			else if (items[j].rating == items[j + 1].rating &&
+				items[j].price > items[j + 1].price)
+		{
+				Item temp = items[j];
+				items[j] = items[j + 1];
+				items[j + 1] = temp;
+		}
+	 }
+	}
+}
