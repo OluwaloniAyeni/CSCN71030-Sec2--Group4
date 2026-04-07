9b98e6f84d9f7ab07a4a2a6d3f9288aa8f606bfb
Oluwaloni <ayenimidun@gmail.com>
2026-04-07 18:12:21 -0400

    add unit test: filterByBudget returns matching items within budget

---
+++ CSCN71030_Tests/CSCN71030_Tests.cpp
@@ -0,0 +1,32 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+extern "C" {
+#include "../CSCN71030_Group4/item.h"	
+#include "../CSCN71030_Group4/search_filtering.h"
+}
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace SearchFilteringTests
+{
+	TEST_CLASS(SearchFilteringTests)
+	{
+	public:
+		
+		TEST_METHOD(FilterByBudget_ReturnsMatchingItems)
+		{
+			Item items[3] = {
+				{1, "Hotel A", "hotel", 100.0, 4.5},
+				{2, "Hotel B", "hotel", 200.0, 4.0},
+				{3, "Hotel C", "hotel", 300.0, 3.5}
+			};
+			int filteredCount = 0;
+			Item* result = filterByBudget(items, 3, "hotel", 0.0f, 200.0f, &filteredCount);
+
+			Assert::IsNotNull(result);
+			Assert::AreEqual(2, filteredCount);
+
+			freeResults(result);
+		}
+	};
+}
