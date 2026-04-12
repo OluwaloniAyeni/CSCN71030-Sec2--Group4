5129dd81d023cf50c18c9eb74eb47e70ecb762fd
Oluwaloni <ayenimidun@gmail.com>
2026-04-12 11:21:30 -0400

    corrected miswordings of function names

--- CSCN71030_Group4/sorting_and_ranking.c
+++ CSCN71030_Group4/sorting_and_ranking.c
@@ -1,7 +1,7 @@
 #include "sorting_and_ranking.h"
 #include <stddef.h>
 
-void sortByPrice(Item* items, int count)
+void sortByPrice(Facility* items, int count)
 {
 
 	if (items == NULL || count <= 1) {
@@ -16,7 +16,7 @@ void sortByPrice(Item* items, int count)
 			if (items[j].price > items[j + 1].price) 
 			
 		{
-				Item temp = items[j];
+				Facility temp = items[j];
 				items[j] = items[j + 1];
 				items[j + 1] = temp;
 		}
@@ -25,7 +25,7 @@ void sortByPrice(Item* items, int count)
 	}
 }
 
-void rankByRating(Item* items, int count)
+void rankByRating(Facility* items, int count)
 {
 	if (items == NULL || count <= 1) {
 		return;
@@ -37,14 +37,14 @@ void rankByRating(Item* items, int count)
 			if (items[j].rating < items[j + 1].rating) 
 			
 		{
-				Item temp = items[j];
+				Facility temp = items[j];
 				items[j] = items[j + 1];
 				items[j + 1] = temp;
 		}
 			else if (items[j].rating == items[j + 1].rating &&
 				items[j].price > items[j + 1].price)
 		{
-				Item temp = items[j];
+				Facility temp = items[j];
 				items[j] = items[j + 1];
 				items[j + 1] = temp;
 		}
