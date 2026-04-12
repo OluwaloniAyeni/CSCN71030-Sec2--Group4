0d4b1ad0eaf0265332b57c129c1bcc1fba299d75
Oluwaloni <ayenimidun@gmail.com>
2026-04-12 11:51:07 -0400

    refactor search_filtering.h to keep budget filtering functions only

--- CSCN71030_Group4/search_filtering.h
+++ CSCN71030_Group4/search_filtering.h
@@ -2,6 +2,5 @@
 
 #include "item.h"
 
-Facility* loadData(const char* filename, int* count);
 Facility* filterByBudget(Facility* list, int count, const char* category, float minBudget, float maxBudget, int* filteredCount);
 void freeResults(Facility* list);
