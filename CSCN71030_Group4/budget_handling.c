7761200fbc0249ed9a39f979a24c672c20fdd9f1
AFSANA ZAMAN <Azaman6489@conestogac.on.ca>
2026-04-12 16:41:39 -0400

    Implemented budget handling module

--- CSCN71030_Group4/budget_handling.c
+++ CSCN71030_Group4/budget_handling.c
@@ -2,17 +2,27 @@
 #include "budget_handling.h"
 
 void processBudget(UserRequest req) {
-    Item items[3] = {
+    Item items[5] = {
         {1, GYM, 15.0, "Gym A"},
         {2, HOTEL, 40.0, "Hotel B"},
-        {3, RESTAURANT, 10.0, "Restaurant C"}
+        {3, RESTAURANT, 10.0, "Restaurant C"},
+        {4, GYM, 25.0, "Gym B"},
+        {5, HOTEL, 60.0, "Hotel C"}
     };
+
+    int found = 0;
 
     printf("\nFiltered Results:\n");
 
-    for (int i = 0; i < 3; i++) {
-        if (items[i].category == req.category && items[i].price <= req.maxBudget) {
+    for (int i = 0; i < 5; i++) {
+        if (items[i].category == req.category &&
+            items[i].price <= req.maxBudget) {
             printf("%s - $%.2f\n", items[i].name, items[i].price);
+            found = 1;
         }
+    }
+
+    if (!found) {
+        printf("No matching results found.\n");
     }
 }
