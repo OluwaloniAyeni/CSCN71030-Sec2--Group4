71b4c84608947afde21326b29112c9f5074a9dcd
Oluwaloni <ayenimidun@gmail.com>
2026-04-13 17:32:29 -0400

    added comments to recommendation.h

--- CSCN71030_Group4/output_display.h
+++ CSCN71030_Group4/output_display.h
@@ -7,7 +7,7 @@
  * @brief Handles displaying results to the user.
  * @details This file declares the function used to display
  * filtered and recommended facilities in a structured format.
- * @author Weiming Wang
+ * @author Evangeline Singh
  * @date 2026-04-13
  */
 
@@ -16,7 +16,7 @@
   * @brief Displays search results.
   * @details This function prints the selected category and budget,
   * and displays the list of facilities in a formatted table.
-  * @author Weiming Wang
+  * @author Evangeline Singh
   * @param request Pointer to user request (category and budget).
   * @param items Pointer to facility list.
   * @param count Number of facilities to display.
