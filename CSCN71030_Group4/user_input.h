676685cdc4cb0735e594858ea72c7522fe6945ea
Oluwaloni <ayenimidun@gmail.com>
2026-04-13 17:38:47 -0400

    added comemnts to user_input.h

--- CSCN71030_Group4/user_input.h
+++ CSCN71030_Group4/user_input.h
@@ -1,6 +1,23 @@
 #pragma once
+
 #include "item.h"
 
+/**
+ * @file user_input.h
+ * @brief Handles user input.
+ * @details This file declares the function used to collect
+ * category selection and budget input from the user.
+ * @author Afsana Zaman
+ * @date 2026-04-13
+ */
+
+
+ /**
+  * @brief Gets user input for category and budget.
+  * @details This function prompts the user to select a category
+  * and enter a maximum budget, then returns the input as a structure.
+  * @author Afsana Zaman
+  * @return UserRequest Structure containing category and budget.
+  * @date 2026-04-13
+  */
 UserRequest getUserInput();
-
-
