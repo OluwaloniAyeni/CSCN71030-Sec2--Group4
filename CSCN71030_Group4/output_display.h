9561e1f5d8b86cca52b062b6b5c62a07c9a0d276
Oluwaloni <ayenimidun@gmail.com>
2026-04-13 17:28:46 -0400

    added commits to output_display.h

--- CSCN71030_Group4/output_display.h
+++ CSCN71030_Group4/output_display.h
@@ -2,7 +2,25 @@
 
 #include "item.h"
 
+/**
+ * @file output_display.h
+ * @brief Handles displaying results to the user.
+ * @details This file declares the function used to display
+ * filtered and recommended facilities in a structured format.
+ * @author Weiming Wang
+ * @date 2026-04-13
+ */
+
+
+ /**
+  * @brief Displays search results.
+  * @details This function prints the selected category and budget,
+  * and displays the list of facilities in a formatted table.
+  * @author Weiming Wang
+  * @param request Pointer to user request (category and budget).
+  * @param items Pointer to facility list.
+  * @param count Number of facilities to display.
+  * @return void
+  * @date 2026-04-13
+  */
 void displayResults(const UserRequest* request, const Facility* items, size_t count);
-
-
-
